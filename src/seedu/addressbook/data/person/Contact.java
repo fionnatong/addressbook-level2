6bae9f38eada7c4b0b77b6b514c39b6c3ff02fab
fionnatong <e0006117@u.nus.edu>
2016-09-06 16:39:09 +0800

    Extract equals method to Contact class

--- src/seedu/addressbook/data/person/Contact.java
+++ src/seedu/addressbook/data/person/Contact.java
@@ -25,4 +25,11 @@ public class Contact {
     public String toString() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Contact // instanceof handles nulls
+                && this.value.equals(((Contact) other).value)); // state check
+    }
 }
